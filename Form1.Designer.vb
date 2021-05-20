526a135893cb0d36a91568a7785daaf2c403f2ef
AHadi Mughal <Mughalhadi13@gmail.com>
2021-05-20 13:15:16 +0500

    Edited Button

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -27,7 +27,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(211, 125)
+        Me.Button1.Location = New System.Drawing.Point(320, 125)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(297, 104)
         Me.Button1.TabIndex = 0
